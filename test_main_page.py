6df3a550358431485982224279587eec08202acb
MaxMarkes1013 <maxyasnogorodskiy@gmail.com>
2020-08-13 18:34:10 +0300

    Implemented update with redirecting page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 
 def test_guest_should_see_login_and_refister_form_and_login_in_url(browser):
@@ -8,3 +9,11 @@ def test_guest_should_see_login_and_refister_form_and_login_in_url(browser):
     page.should_be_login_url()
     page.should_be_login_form()
     page.should_be_register_form()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
